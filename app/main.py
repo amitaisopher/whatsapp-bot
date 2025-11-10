2313651b5d6604fe646b7b9dfb7b03a7624103fe
Amitai Sopher <amitaisopher@gmail.com>
2025-11-10 13:01:31 +0200

    Refactor Dockerfile and main application; update command to run app using uvicorn and set asyncio event loop policy

--- app/main.py
+++ app/main.py
@@ -3,7 +3,10 @@ from app.create_app import create_app
 
 def main():
     app = create_app()
+    import uvloop
     import uvicorn
+    import asyncio
+    asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 
     uvicorn.run(app, host="0.0.0.0", port=8000)
 
